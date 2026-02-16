8b768280170730e89a09a6f3a0dc237aa753abed
Brandon Pettenati <3brandonjp@gmail.com>
2026-02-16 18:25:06 -0500

    Simple Router Complete

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
-import App from "./App.jsx";
+
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -16,6 +18,8 @@ createRoot(document.getElementById("root")).render(
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/header";
+
+function Contact() {
+    return (
+        <>
+            <Header />
+            <h1>Contact Page</h1>
+        </>
+    )
+}
+export default Contact;
